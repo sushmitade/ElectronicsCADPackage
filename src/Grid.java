2ca92b75abddf09b2908a6568cac2ca97b04e876
sushmitade <sde@stevens.edu>
2015-12-02 23:54:26 -0500

    Import files and adding some things to initialize the class.

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,22 @@
 
-public class Grid {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+public class Grid extends JFrame implements ActionListener {
+
+	int r;
+	JButton[] button1 = new JButton[30];
+
+	Font font = null;
+	JTextField textField = null;
+
+	public static void main(String[] args) {
+		new Grid();
 }
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+}
+	
